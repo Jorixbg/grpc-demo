01492a938ac1cba5fdac0c7325eb393b797d32cd
Tushka Dermendzhieva <tushka.dermendzhieva@scalefocus.com>
2019-12-10 19:29:24 +0200

    Add more comments in the code

--- src/main/java/com/grpc/chat/ChatServiceImpl.java
+++ src/main/java/com/grpc/chat/ChatServiceImpl.java
@@ -7,17 +7,14 @@ import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ChatServiceImpl extends ChatServiceGrpc.ChatServiceImplBase {
-    //clients
+    //clients that are subscribed
     private static Set<StreamObserver<ChatMessageFromServer>> observers = ConcurrentHashMap.newKeySet();
 
     @Override
-    /*
-        Since we have bi directional streaming we have Stream Observer as return type
-        and the response as parameter *** responseObserver what the server needs to send to the client
-        and Need to use a StreamObserver to stream data back to the client.
-        The return type is actually what the client sends to the server
-        and the argument is what the server sends to the client.
-     */
+    /*since we have bi- directional streaming we have StreamObserver as return type and input param
+    StreamObserver<ChatMessageFromServer> is what the server needs to send to the client and the return type is what the client send to the server
+    You are returning an object that is actually getting data from the client to the server radther than sedning data back from
+    the server to the client this is important and I will show you why this is done this way  */
     public StreamObserver<ChatMessage> chat(StreamObserver<ChatMessageFromServer> responseObserver) {
         observers.add(responseObserver);
 
@@ -27,10 +24,12 @@ public class ChatServiceImpl extends ChatServiceGrpc.ChatServiceImplBase {
          */
         return new StreamObserver<ChatMessage>() {
 
+           // When the client sends something to the server this onNext method is where we are recieving the data from client to the server,
+           // and when we receive a chat message what we are going to do is to send it to all of the existing connections that we have
             @Override
             public void onNext(ChatMessage value) { // Called when a client sends a message to the server
                 System.out.println(value);
-                ChatMessageFromServer message = ChatMessageFromServer.newBuilder()
+                ChatMessageFromServer message = ChatMessageFromServer.newBuilder()//builder pattern is used everywhere in gRPC
                         .setMessage(value)
                         .setTimestamp(Timestamp.newBuilder().setSeconds(System.currentTimeMillis() / 1000))
                         .build();
